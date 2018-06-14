2e125d84b52264bbb7ddd10b47380f67130afad1
Tom Klaver <tomklav@gmail.com>
2018-06-14 14:17:41 +0200

    add raw domain to certbot

--- nginx/certbot.sh
+++ nginx/certbot.sh
@@ -6,20 +6,20 @@ done
 
 renew () {
     echo "Renewing certificate..."
-    ( set -x; certbot renew --renew-hook  "cp -r /etc/letsencrypt/live/www.${DOMAIN}/*.pem /cert")
+    ( set -x; certbot renew --renew-hook  "cp -r /etc/letsencrypt/live/${DOMAIN}/*.pem /cert")
     if [ $? -ne 0 ]; then
         echo "WARNING: Certbot failed to renew certificate"
     fi
 }
 
-if [ ! -f /etc/letsencrypt/renewal/www.${DOMAIN}.conf ]; then
+if [ ! -f /etc/letsencrypt/renewal/${DOMAIN}.conf ]; then
     echo "No existing certbot config found, creating new certificate..."
-    ( set -x; certbot certonly --webroot -w /acme -n --agree-tos --email ${ADMIN_EMAIL} -d www.${DOMAIN} --expand )
+    ( set -x; certbot certonly --webroot -w /acme -n --agree-tos --email ${ADMIN_EMAIL} -d ${DOMAIN} -d www.${DOMAIN} --expand )
     if [ $? -ne 0 ]; then
         echo "WARNING: Certbot failed to create certificate"
         exit 1
     else
-        cp -r /etc/letsencrypt/live/www.${DOMAIN}/*.pem /cert
+        cp -r /etc/letsencrypt/live/${DOMAIN}/*.pem /cert
     fi
 else
     renew
